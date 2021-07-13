e7865eb5f02521a477eb29515f1c98a2d06d7a0d
Nicole Uy <marvienicolebtw@hotmail.com>
2021-07-13 10:12:32 +0200

    editando TablaUsuarios y App.js para aceptar lista como propiedad

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TablaUsuarios from './tables/TablaUsuarios';
 
 const App = () => {
-  return (
-      <div className="container mt-4">
-        <h1>Tutorial React Hooks</h1>
-        <div className="row">
-          <div className="col-md-4 mt-4">
-            <h2>Agregar usuario</h2>
-          </div>
-          <div className="col-md-8 mt-4">
-            <h2>Ver usuarios</h2>
-          </div>
+
+    const datosUsuario = [
+        { id: 1, nombre: 'Edu', apellido: 'Lázaro' },
+        { id: 2, nombre: 'Ana', apellido: 'Rodríguez' },
+        { id: 3, nombre: 'Marcos', apellido: 'González' },
+    ];
+
+    const [usuarios, setUsuarios] = useState(datosUsuario);
+
+    return (
+        <div className="container">
+            <h1>Tutorial React Hooks</h1>
+            <div className="row">
+                <div className="col-md-4">
+                    <h2>Agregar usuario</h2>
+                </div>
+                <div className="col-md-4">
+                    <h2>Ver usuarios</h2>
+                    <TablaUsuarios usuarios={usuarios} />
+                </div>
+            </div>
         </div>
-      </div>
-  )
+    );
 }
 
 export default App;
--- src/tablas/TablaUsuario.js
+++ src/tablas/TablaUsuario.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const TablaUsuarios = () => (
+const TablaUsuarios = (props) => (
     <table className="table mt-4">
         <thead className="thead-dark">
         <tr>
@@ -10,14 +10,22 @@ const TablaUsuarios = () => (
         </tr>
         </thead>
         <tbody>
-        <tr>
-            <td>Dukecito</td>
-            <td>Nukemito</td>
-            <td>
-                <button type="button" className="btn btn-primary btn-sm m-1">Editar</button>
-                <button type="button" className="btn btn-primary btn-sm m-1">Eliminar</button>
-            </td>
-        </tr>
+        {props.usuarios.length > 0 ? (
+            props.usuarios.map((usuario) => (
+                <tr key={usuario.id}>
+                    <td>{usuario.nombre}</td>
+                    <td>{usuario.apellido}</td>
+                    <td>
+                        <button type="button" className="btn btn-primary btn-sm m-1">Editar</button>
+                        <button type="button" className="btn btn-primary btn-sm m-1">Eliminar</button>
+                    </td>
+                </tr>
+            ))
+        ) : (
+            <tr>
+                <td colSpan={3}>No se han agregado usuarios</td>
+            </tr>
+        )}
         </tbody>
     </table>
 )
