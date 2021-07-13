ffd38607c55539474ac5c1cb94dc6c3907e7cbbe
Nicole Uy <marvienicolebtw@hotmail.com>
2021-07-13 09:50:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="container mt-4">
+        <h1>Tutorial React Hooks</h1>
+        <div className="row">
+          <div className="col-md-4 mt-4">
+            <h2>Agregar usuario</h2>
+          </div>
+          <div className="col-md-8 mt-4">
+            <h2>Ver usuarios</h2>
+          </div>
+        </div>
+      </div>
+  )
 }
 
 export default App;
